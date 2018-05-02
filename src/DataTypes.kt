d02a9543a26981aae3620f2645d4c3807b7e39f8
kajal <kajalpadhy@gmail.com>
2018-05-02 17:16:06 -0500

    about datatype

---
+++ src/DataTypes.kt
@@ -0,0 +1,14 @@
+
+fun main(args:Array<String>){
+
+     val name="kajal"
+     val age=28
+     val dep:String?
+     dep="Software engineering"
+     println("name:"+ name)
+     println("age:"+ age)
+     println("department:"+ dep)
+     val pi:Double=3.4
+
+
+}
